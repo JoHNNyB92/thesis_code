f6bd4d16cb14bb6682d2daa7a3b41151480516cc
avgeros <j.avgeros1992@hotmail.gr>
2019-08-26 14:54:33 +0300

    added comments

--- thesis/tensorflow_parse_files/tensorflow_parser.py
+++ thesis/tensorflow_parse_files/tensorflow_parser.py
@@ -25,7 +25,10 @@ def put_inputs_in_map(node):
         inputs.append(nodes.handler.entitiesHandler.node_map[rest])
     nodes.handler.entitiesHandler.node_map[node.name].inputs=inputs
 
-def parse_pbtxt(path,part_name):
+#Parse file and iterate through all nodes produced that belong to the neural network,call function to store them
+#in a map.It also call function to handle the node based on the respective case.
+def parse_pbtxt(path):
+    #Protocol file parse and read.
     graph_def = graph_pb2.GraphDef()
     with open(path, "rb") as f:
         text_format.Merge(f.read(), graph_def)
@@ -33,41 +36,44 @@ def parse_pbtxt(path,part_name):
         put_in_map(node)
     for node in graph_def.node:
         put_inputs_in_map(node)
-    #TODO: PER NETWORK THIS SHOULD BE DONE
     curr=nodes.handler.entitiesHandler.current_network
+    #Iterate through all nodes.
     for e in nodes.handler.entitiesHandler.node_map.keys():
         nodes.handler.entitiesHandler.handle_different_layer_cases(e)
+    #Some layer can be part of a bigger layer,thus those layers should be removed from in here.
     nodes.handler.entitiesHandler.clear_layers()
-    inputs=[]
-    outputs=[]
+    #For each layer identified,find input and output layers.
     for elem in nodes.handler.entitiesHandler.data.annConfiguration.networks[curr].layer.keys():
         print("=============================================")
         print("Finding input for layer=",elem)
         nodes.handler.entitiesHandler.data.annConfiguration.networks[curr].layer[elem].find_input_layer()
         print("=============================================")
-    for elem in nodes.handler.entitiesHandler.data.annConfiguration.networks[curr].layer.keys():
-        #If only one input and that input is placeholder then it an input layer
-        if nodes.handler.entitiesHandler.data.annConfiguration.networks[curr].layer[elem].is_input==True and \
-            len(nodes.handler.entitiesHandler.data.annConfiguration.networks[curr].layer[elem].previous_layer)==1:
-            inputs.append(elem)
     result=print_info.print_topology()
     if result!="success":
         print("LOGGING:Parsing failed.")
     return result
 
+#Initial call for parsing pbtxt
 def begin_parsing(name,pbtxt_file,log_file,counter):
     nodes.handler.entitiesHandler=handle_entities()
     part_name=name.replace(".py","")
     rdfWrapper.log_file=log_file
+    #Initalize some basic information for the neural network.
+    #The data class,storing information for inserting into annetto
     rdfWrapper.new_init_data(part_name,counter)
+    #New network name
     rdfWrapper.new_init_new_network(part_name)
+    #New evaluation function
     rdfWrapper.new_init_new_evaluation(part_name+"_eval",part_name)
     result=parse_pbtxt(pbtxt_file,part_name)
     return (result,nodes.handler.entitiesHandler)
 
+#Function to insert into the annetto triple store database
 def insert_in_annetto():
     cnt=rdfWrapper.insert_ann_graph()
+    #Reset variables used for different names on multiple networks inserted.
     rdfWrapper.reset_vars()
+    #Reset also entitieshandler class
     nodes.handler.entitiesHandler=""
     return cnt
 
